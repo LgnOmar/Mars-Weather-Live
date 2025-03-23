0dfed12831b95d13c811669923ec735afd858a1f
LAGGOUNE Omar <dogie4bogie@gmail.com>
2025-03-23 20:37:09 +0100

    pretty much all is done
    im only left with the toggle, and a little bug with the wind direction compass to fix and we're good to go.

--- script.js
+++ script.js
@@ -4,20 +4,79 @@ const API_URL = 'http://localhost:3000/api/weather';
 
 
 const previousWeatherToggle = document.querySelector(".show-previous-weather");
-
-
 const previousWeather= document.querySelector('.previous-weather');
+const currentSolElement = document.querySelector('[data-current-sol]')
+const currentDateElement = document.querySelector('[data-current-date]')
+const currentTempHighElement  = document.querySelector('[data-current-temp-high]')
+const currentTemplowElement  = document.querySelector('[data-current-temp-low]')
+const windSpeedElement = document.querySelector('[data-wind-speed]')
+const windDirectionText = document.querySelector('[data-wind-direction-text]')
+const windDirectionArrow = document.querySelector('[data-wind-direction-arrow]')
+
+
+const previousSolTemplate = document.querySelector('[data-previous-sol-template]');
+const previousSolContainer = document.querySelector('[data-previous-sols]');
+
 
 previousWeatherToggle.addEventListener('click', () => {
     previousWeather.classList.toggle('show-weather')
 })
 
+let selectedSolIndex
+
+getWeather().then(sols =>{
+    selectedSolIndex = sols.length -1;
+    displaySelectedSol(sols);
+    displayPreviousSols(sols)
+});
 
 
-getWeather();
+function displaySelectedSol(sols){
+    const selectedSol = sols[selectedSolIndex]
+    currentSolElement.innerText = selectedSol.sol;
+    currentDateElement.innerText = displayDate(selectedSol.date);
+    currentTempHighElement.innerText = displayTemperature(selectedSol.maxTemp);
+    currentTemplowElement.innerText = displayTemperature(selectedSol.minTemp);
+    windSpeedElement.innerText = displaySpeed(selectedSol.windSpeed);
+    windDirectionText.innerText = selectedSol.windDirectionCardinal;
+    windDirectionArrow.style.setProperty('--direction', `${selectedSol.windDirectionDegrees}deg`)
+}
+
+function displayPreviousSols(sols){
+    previousSolContainer.innerHTML ='';
+    sols.forEach((solData, index) =>{
+        const solContainer = previousSolTemplate.content.cloneNode(true)
+        solContainer.querySelector('[data-sol]').innerText = solData.sol;
+        solContainer.querySelector('[data-date]').innerText = displayDate(solData.date);
+        solContainer.querySelector('[data-temp-high]').innerText = displayTemperature(solData.maxTemp);
+        solContainer.querySelector('[data-temp-low]').innerText = displayTemperature(solData.minTemp);
+        solContainer.querySelector('[data-select-button]').addEventListener('click',() =>{
+            selectedSolIndex = index;
+            displaySelectedSol(sols)
+        })
+        previousSolContainer.appendChild(solContainer);
+    })
+}
+
+function displayTemperature(temperature){
+    return Math.round(temperature)
+}
+function displaySpeed(speed){
+    return Math.round(speed)
+}
+
+function displayDate(date){
+    return date.toLocaleDateString(
+        undefined,
+        {
+            day: 'numeric', month: 'long'
+        }
+    )
+}
+
 
 function getWeather(){
-    fetch(API_URL)
+    return fetch(API_URL)
     .then(res => res.json())
     .then(data =>{
         const {
@@ -25,7 +84,7 @@ function getWeather(){
             validity_checks,
             ...solData
         } = data
-        const temp = Object.entries(solData).map(([sol,data]) =>{
+        return Object.entries(solData).map(([sol,data]) =>{
             return {
                 sol: sol,
                 maxTemp: data.AT?.mx,
@@ -36,7 +95,6 @@ function getWeather(){
                 date: data.First_UTC ? new Date(data.First_UTC) : null
             }
         })
-        console.log(temp)
     })
 
 }
